a23f5255224cead1fd19468d5626326254820a12
sebastianf101 <sebastianf101@gmail.com>
2025-02-18 18:43:17 -0300

    Prueba act fork

---
+++ Instal/git related.R
@@ -0,0 +1,38 @@
+
+# Para confirmar que el setup Github es correcto verificar que el siguiente comando devuelve:
+usethis::git_sitrep()
+
+# ── Git global (user) 
+# • Name: "sebastianf101"
+# • Email: "sebastianf101@gmail.com"
+# • Global (user-level) gitignore file: C:/Users/sferro/.gitignore
+# • Vaccinated: TRUE
+# • Default Git protocol: "https"
+# • Default initial branch name: "master" and "main"
+# 
+# ── GitHub user 
+# • Default GitHub host: "https://github.com"
+# • Personal access token for "https://github.com": <discovered>
+#   • GitHub user: "sebastianf101"
+# • Token scopes: "gist", "read:project", "repo", "user", "workflow", and "write:packages"
+# • Email(s): "sebastianf101@gmail.com (primary)" and "sebastianf101@hotmail.com"
+# 
+# ── Active usethis project: "C:/Users/sferro/Documents/Trabajo/Projects/SmartModel/SmartModelV2" ──
+# 
+# 
+# ── Git local (project) 
+# • Name: "sebastianf101"
+# • Email: "sebastianf101@gmail.com"
+# • Default branch: "main"
+# • Current local branch → remote tracking branch:
+#   "main" → "upstream/main"
+# 
+# ── GitHub project 
+# • Type = "fork"
+# • Host = "https://github.com"
+# • Config supports a pull request = TRUE
+# • origin = "sebastianf101/SmartModelV2" (can push) = fork of "sferro-besmart/SmartModelV2"
+# • upstream = "sferro-besmart/SmartModelV2" (can push)
+# ℹ "origin" is a fork of "sferro-besmart/SmartModelV2", which is configured as the "upstream" remote.
+# ℹ Read more about the GitHub remote configurations that usethis supports at:
+#   <https://happygitwithr.com/common-remote-setups.html>.
