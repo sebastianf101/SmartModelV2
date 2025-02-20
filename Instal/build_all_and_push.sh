e987e500eebede70e9bd490b99ac3481f608fedb
sebastianf101 <sebastianf101@gmail.com>
2025-02-20 18:37:30 -0300

    push a dockerhub y ghcr en build_all_and_push

--- Instal/build_all_and_push.sh
+++ Instal/build_all_and_push.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 # Bash script para construir imagen desde cero y salvarla
+# Setear antes las env vars con los paswords
 # Ojo asegurarse de que el siguiente cd sea el directorio raiz del proyecto!!!
 cd /c/Users/sferro/Documents/Trabajo/Projects/SmartModel/SmartModelV2/
-echo "Login a docker.io"
-docker login  -u "sebastianf101" docker.io
 echo "Inicio construcción de imagen bsm-studio-paq:v2.7"
+echo $DOCKER_PWD | docker login  -u "sebastianf101" --password-stdin docker.io
 docker build -t bsm-studio-paq -f Instal/Dockerfile.bsm-studio-paq.v2 . #--no-cache 
 docker tag bsm-studio-paq sebastianf101/bsm-studio-paq:v2.7
 # Sólo se permite un sólo repo privado en Docker Hub
@@ -15,10 +15,13 @@ docker tag bsm-studio-ssh sebastianf101/bsm-studio:v10.3
 echo "Fin de construcción de imagen"
 echo "Inicio subida a docker Hub"
 docker push sebastianf101/bsm-studio:v10.3
+docker logout docker.io
 echo "Fin subida a docker Hub"
 echo "Inicio subida a GitHub Container Registry"
+echo $GHCR_PUSH_PWD | docker login -u sferro-besmart --password-stdin ghcr.io 
 docker tag bsm-studio-ssh ghcr.io/sferro-besmart/smartmodelv2:v10.3.0
 docker push ghcr.io/sferro-besmart/smartmodelv2:v10.3.0
+docker logout ghcr.io
 echo "Fin subida a GitHub Container Registry"
 #docker save sebastianf101/bsm-studio:v10.3 | gzip -9 > Instal/bsm-studio-v10.3.tar.gz
 
