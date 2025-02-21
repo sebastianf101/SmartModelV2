a1c8360c8bc2b7ea8411cc46ae65c5c6318c0df2
sebastianf101 <sebastianf101@gmail.com>
2025-02-21 20:55:09 -0300

    Adding of BSM_IMAGE

--- Instal/Automat/envvars_defaults.sh
+++ Instal/Automat/envvars_defaults.sh
@@ -4,7 +4,8 @@
 export BSM_USER="user"
 export BSM_VERSION="10.3"
 export BSM_DIR="/home/$BSM_USER/Documents/besmart/$BSM_VERSION"
-export BSM_RETRIES=1
+export BSM_IMAGE="ghcr.io/sferro-besmart/smartmodelv2:v10.3.0"
+export BSM_RETRIES=3
 export BSM_PWD="smartmodel"
 export BSM_CPU_CORES_MIN="2"
 export BSM_MEMORY_MIN="4G"
