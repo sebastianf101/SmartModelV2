dfc9c3ac801153bdb328e05c764840155e737114
sebastianf101 <sebastianf101@gmail.com>
2025-02-27 19:06:34 -0300

    Pulido de buid_all_and_push.sh

--- Instal/build_all_and_push.sh
+++ Instal/build_all_and_push.sh
@@ -1,30 +1,56 @@
 #! /bin/bash
 # Bash script para construir imagen desde cero y salvarla
-# Setear antes las env vars con los paswords
-# Ojo asegurarse de que el siguiente cd sea el directorio raiz del proyecto!!!
+# Setear antes las env vars con los paswords!
+# Para detenerse al mínimo error.
+set -e
+set -o pipefail
+
+# Ojo asegurarse de que estoy parado en el siguiente cd sea el directorio raiz del proyecto!!!
 cd /c/Users/sferro/Documents/Trabajo/Projects/SmartModel/SmartModelV2/
-echo "Inicio construcción de imagen bsm-studio-paq:v2.7"
-echo $DOCKER_PWD | docker login  -u "sebastianf101" --password-stdin docker.io
-docker build -t bsm-studio-paq -f Instal/Dockerfile.bsm-studio-paq.v2 . #--no-cache 
-docker tag bsm-studio-paq sebastianf101/bsm-studio-paq:v2.7
+#
+
+# Function to check and prompt for env var
+check_and_set_env_var() {
+    local var_name=$1
+    local var_value
+
+    eval var_value=\$"$var_name"
+
+    if [ -z "$var_value" ]; then
+        read -p "Enter value for $var_name: " var_value
+        export "$var_name"="$var_value"
+    fi
+}
+
+# Check and prompt for env vars. 
+check_and_set_env_var "IMAGE_VERSION"
+check_and_set_env_var "DOCKER_PWD"
+check_and_set_env_var "GHCR_PUSH_PWD"
+
+echo "Versión de Imagen: $IMAGE_VERSION"
+echo "Inicio construcción de imagen bsm-studio-paq"
+docker build -t bsm-studio-paq -f Instal/Dockerfile.bsm-studio-paq . #--no-cache 
+docker tag bsm-studio-paq sebastianf101/bsm-studio-paq 
+echo "Fin de construcción de imagen bsm-studio-paq"
 # Sólo se permite un sólo repo privado en Docker Hub
-# docker push sebastianf101/bsm-studio-paq:v2.7
-echo "Inicio construcción de imagen bsm-studio:v10.3"
-docker build -t bsm-studio-ssh -f Instal/Dockerfile.bsm-studio.v2 --progress=plain . #--no-cache 
-docker tag bsm-studio-ssh sebastianf101/bsm-studio:v10.3
-echo "Fin de construcción de imagen"
-echo "Inicio subida a docker Hub"
-docker push sebastianf101/bsm-studio:v10.3
+echo "Inicio construcción de imagen bsm-studio"
+docker build -t bsm-studio -f Instal/Dockerfile.bsm-studio --progress=plain . #--no-cache 
+echo "Fin de construcción de imagen bsm-studio"
+echo "Inicio subida a docker Hub de imagen sebastianf101/bsm-studio:$IMAGE_VERSION"
+echo $DOCKER_PWD | docker login  -u "sebastianf101" --password-stdin docker.io 
+docker tag bsm-studio sebastianf101/bsm-studio:$IMAGE_VERSION
+docker push sebastianf101/bsm-studio:$IMAGE_VERSION
 docker logout docker.io
 echo "Fin subida a docker Hub"
-echo "Inicio subida a GitHub Container Registry"
+echo "Inicio subida a GitHub Container Registry de imagen ghcr.io/sferro-besmart/smartmodelv2:$IMAGE_VERSION"
 echo $GHCR_PUSH_PWD | docker login -u sferro-besmart --password-stdin ghcr.io 
-docker tag bsm-studio-ssh ghcr.io/sferro-besmart/smartmodelv2:v10.3.0
-docker push ghcr.io/sferro-besmart/smartmodelv2:v10.3.0
+docker tag bsm-studio ghcr.io/sferro-besmart/smartmodelv2:$IMAGE_VERSION
+docker push ghcr.io/sferro-besmart/smartmodelv2:$IMAGE_VERSION
 docker logout ghcr.io
 echo "Fin subida a GitHub Container Registry"
-#docker save sebastianf101/bsm-studio:v10.3 | gzip -9 > Instal/bsm-studio-v10.3.tar.gz
-
+echo "Inicio generación imagen.tar.gz"
+docker save bsm-studio | gzip -9 > Instal/Automat/bsm-studio.tar.gz
+echo "Fin generación imagen.tar.gz"
 
 
 
