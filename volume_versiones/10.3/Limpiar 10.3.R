ca30f7bc69f28d8bf23c8428a6f41759c434e9cd
sebastianf101 <sebastianf101@gmail.com>
2026-02-10 23:25:11 -0300

    Use BSM_DIR for cleanup paths

--- volume_versiones/10.3/Limpiar 10.3.R
+++ volume_versiones/10.3/Limpiar 10.3.R
@@ -2,14 +2,11 @@
 # Ojo que limpia todos los archivos de las carpetas nombradas abajo.
 # Conserva las otras carpetas.
 #
-setwd("~/Documents/besmart/10.3/")
 version_bsm <- "10.3"
-bsm_path <- fs::path("~/Documents/besmart", version_bsm)
+bsm_dir <- Sys.getenv("BSM_DIR", unset = fs::path("~/Documents/besmart", version_bsm))
+bsm_path <- fs::path(bsm_dir)
+setwd(bsm_path)
 options(digits=3) # Fija en la sesión el nro. de digitos decimales a visualizar.
-
-msg <- paste("Esta a punto de actualizar a la version", version_bsm,
-             "y de borrar los contenidos de los directorios Auxil, Logs, Reportes y Trabajo.",
-             "Esta seguro de querer continuar?. Responda S o N (por Si o No)", collapse = " ")
 
 unlink("Auxil", recursive = TRUE, force = TRUE)
 unlink("Logs", recursive = TRUE, force = TRUE)
@@ -19,6 +16,6 @@ fs::path(bsm_path, "Auxil") |> fs::dir_create()
 fs::path(bsm_path, "Logs") |> fs::dir_create()
 fs::path(bsm_path, "Reportes") |> fs::dir_create()
 fs::path(bsm_path, "Trabajo") |> fs::dir_create()
-fs::path(bsm_path, "Scripts/Tejer Cuadernos.R")
+
 
 
